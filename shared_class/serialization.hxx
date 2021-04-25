50d9cc1f4eb58966b8aa839e9ae2325b6063156c
werto87 <werto87>
2021-04-25 13:38:43 +0200

    init shared_class

---
+++ shared_class/serialization.hxx
@@ -0,0 +1,10 @@
+#ifndef A454B6F4_5E02_4669_955C_18F207EE36D6
+#define A454B6F4_5E02_4669_955C_18F207EE36D6
+
+#include "confu_boost/confuBoost.hxx"
+
+BOOST_FUSION_DEFINE_STRUCT ((serialization), JoinChannel, (std::string, channel))
+
+BOOST_SERIALIZATION_BOILER_PLATE (serialization::JoinChannel)
+
+#endif /* A454B6F4_5E02_4669_955C_18F207EE36D6 */
