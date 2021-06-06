40d0b91c2a95667e5cb6e0ed24e7a3100349ce4f
werto87 <huhulasmichinruhe@web.de>
2021-06-06 23:45:35 +0200

    update to confu_json

--- game_01_shared_class/serialization.hxx
+++ game_01_shared_class/serialization.hxx
@@ -1,6 +1,5 @@
 #ifndef A454B6F4_5E02_4669_955C_18F207EE36D6
 #define A454B6F4_5E02_4669_955C_18F207EE36D6
-
 #include <boost/algorithm/string.hpp>
 #include <boost/fusion/adapted/struct/adapt_struct.hpp>
 #include <boost/fusion/adapted/struct/define_struct.hpp>
@@ -24,11 +23,10 @@
 #include <boost/hana/pair.hpp>
 #include <boost/hana/tuple.hpp>
 #include <boost/hana/type.hpp>
+#include <boost/json.hpp>
 #include <boost/mpl/for_each.hpp>
 #include <boost/mpl/if.hpp>
 #include <boost/mpl/range_c.hpp>
-#include <cereal/archives/json.hpp>
-#include <cereal/types/vector.hpp>
 #include <cstddef>
 #include <durak/game.hxx>
 #include <iostream>
@@ -36,71 +34,6 @@
 #include <sstream>
 #include <string>
 #include <variant>
-
-template <class Archive, typename T>
-void
-serializationHelper (Archive &ar, T &t)
-{
-  boost::fusion::for_each (boost::mpl::range_c<unsigned, 0, boost::fusion::result_of::size<T>::value> (), [&] (auto index) { ar &cereal::make_nvp (boost::fusion::extension::struct_member_name<T, index>::call (), boost::fusion::at_c<index> (t)); });
-}
-
-// use SERIALIZATION_BOILER_PLATE in a namespace
-// clang-format off
-#ifndef SERIALIZATION_BOILER_PLATE
-#define SERIALIZATION_BOILER_PLATE(NAMESPACE_WITH_TYPE)                                     \
-void prologue(cereal::JSONOutputArchive &ar, const NAMESPACE_WITH_TYPE &) {                 \
-  auto fullName = std::vector<std::string>{};                                               \
-  auto typeWithNamespace = boost::typeindex::type_id<NAMESPACE_WITH_TYPE>().pretty_name();  \
-  boost::algorithm::split(fullName, typeWithNamespace, boost::is_any_of("::"));             \
-  ar.setNextName(fullName.back().c_str());                                                  \
-  ar.startNode();                                                                           \
-}                                                                                           \
-void epilogue(cereal::JSONOutputArchive &ar, const NAMESPACE_WITH_TYPE &) {                 \
-  ar.finishNode();                                                                          \
-}                                                                                           \
-  template <class Archive>                                                                  \
-  void                                                                                      \
-  serialize (Archive &ar, NAMESPACE_WITH_TYPE &t)                                           \
-  {                                                                                         \
-    serializationHelper (ar, t);                                                            \
-  }                                                                                         \
-
-#endif
-// clang-format on
-
-BOOST_FUSION_DEFINE_STRUCT ((shared_class), MyClass, (int, x) (int, y) (int, z))
-namespace shared_class
-{
-SERIALIZATION_BOILER_PLATE (shared_class::MyClass)
-} // namespace shared_class
-
-template <typename T>
-std::string
-toJson (T const &t)
-{
-  std::stringstream ss{}; // any stream can be used
-  {
-    cereal::JSONOutputArchive oarchive (ss); // Create an output archive
-    oarchive (t);
-  }
-  return ss.str ();
-}
-
-// TODO think about search for the type by name in some lookup so we dont have
-// to specify the template parameter
-template <typename T>
-T
-toObject (std::string const &objectAsString)
-{
-  std::stringstream ss{};
-  T t{};
-  {
-    ss << objectAsString;
-    cereal::JSONInputArchive iarchive (ss);
-    iarchive (t);
-  }
-  return t;
-}
 
 BOOST_FUSION_DEFINE_STRUCT ((shared_class), JoinChannel, (std::string, channel))
 BOOST_FUSION_DEFINE_STRUCT ((shared_class), JoinChannelSuccess, (std::string, channel))
@@ -172,86 +105,6 @@ BOOST_FUSION_DEFINE_STRUCT ((shared_class), DurakGameOverWon, )
 BOOST_FUSION_DEFINE_STRUCT ((shared_class), DurakGameOverLose, )
 BOOST_FUSION_DEFINE_STRUCT ((shared_class), DurakGameOverDraw, )
 
-namespace shared_class
-{
-SERIALIZATION_BOILER_PLATE (shared_class::JoinChannel)
-SERIALIZATION_BOILER_PLATE (shared_class::JoinChannelSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::JoinChannelError)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateAccount)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateAccountSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateAccountError)
-SERIALIZATION_BOILER_PLATE (shared_class::LoginAccount)
-SERIALIZATION_BOILER_PLATE (shared_class::LoginAccountSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::LoginAccountError)
-SERIALIZATION_BOILER_PLATE (shared_class::BroadCastMessage)
-SERIALIZATION_BOILER_PLATE (shared_class::BroadCastMessageSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::BroadCastMessageError)
-SERIALIZATION_BOILER_PLATE (shared_class::LeaveChannel)
-SERIALIZATION_BOILER_PLATE (shared_class::LeaveChannelSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::LeaveChannelError)
-SERIALIZATION_BOILER_PLATE (shared_class::Message)
-SERIALIZATION_BOILER_PLATE (shared_class::LogoutAccount)
-SERIALIZATION_BOILER_PLATE (shared_class::LogoutAccountSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::LogoutAccountError)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateGameLobby)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateGameLobbySuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateGameLobbyError)
-SERIALIZATION_BOILER_PLATE (shared_class::JoinGameLobby)
-SERIALIZATION_BOILER_PLATE (shared_class::JoinGameLobbySuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::JoinGameLobbyError)
-SERIALIZATION_BOILER_PLATE (shared_class::UserInGameLobby)
-SERIALIZATION_BOILER_PLATE (shared_class::UsersInGameLobby)
-SERIALIZATION_BOILER_PLATE (shared_class::SetMaxUserSizeInCreateGameLobby)
-SERIALIZATION_BOILER_PLATE (shared_class::MaxUserSizeInCreateGameLobby)
-SERIALIZATION_BOILER_PLATE (shared_class::SetMaxUserSizeInCreateGameLobbyError)
-SERIALIZATION_BOILER_PLATE (shared_class::LeaveGameLobby)
-SERIALIZATION_BOILER_PLATE (shared_class::LeaveGameLobbySuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::LeaveGameLobbyError)
-SERIALIZATION_BOILER_PLATE (shared_class::WantToRelog)
-SERIALIZATION_BOILER_PLATE (shared_class::RelogTo)
-SERIALIZATION_BOILER_PLATE (shared_class::RelogToError)
-SERIALIZATION_BOILER_PLATE (shared_class::RelogToSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateAccountCancel)
-SERIALIZATION_BOILER_PLATE (shared_class::LoginAccountCancel)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateGame)
-SERIALIZATION_BOILER_PLATE (shared_class::StartGame)
-SERIALIZATION_BOILER_PLATE (shared_class::CreateGameError)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAttack)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAttackSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAttackError)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefend)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendError)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAttackPass)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAttackPassSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAttackPassError)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAssistPass)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAssistPassSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAssistPassError)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendPass)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendPassSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendPassError)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAskDefendWantToTakeCards)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAskDefendWantToTakeCardsAnswer)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAskDefendWantToTakeCardsAnswerSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakAskDefendWantToTakeCardsAnswerError)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendWantsToTakeCardsFromTableDoYouWantToAddCards)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendWantsToTakeCardsFromTableDoYouWantToAddCardsAnswer)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendWantsToTakeCardsFromTableDoneAddingCards)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendWantsToTakeCardsFromTableDoneAddingCardsSuccess)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakDefendWantsToTakeCardsFromTableDoneAddingCardsError)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakGameOverWon)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakGameOverLose)
-SERIALIZATION_BOILER_PLATE (shared_class::DurakGameOverDraw)
-}
-namespace durak
-{
-SERIALIZATION_BOILER_PLATE (durak::Type)
-SERIALIZATION_BOILER_PLATE (durak::PlayerRole)
-SERIALIZATION_BOILER_PLATE (durak::Card)
-SERIALIZATION_BOILER_PLATE (durak::PlayerData)
-SERIALIZATION_BOILER_PLATE (durak::GameData)
-}
 // clang-format off
 namespace shared_class{
 static boost::hana::tuple<
--- test/constant.hxx
+++ test/constant.hxx
@@ -1,6 +1,70 @@
 #ifndef FE334B5B_FA67_454D_A6F5_A1CBF7D02BB7
 #define FE334B5B_FA67_454D_A6F5_A1CBF7D02BB7
+#include "game_01_shared_class/serialization.hxx"
+#include <boost/fusion/adapted/struct/adapt_struct.hpp>
+#include <boost/fusion/include/is_sequence.hpp>
+#include <boost/fusion/support/is_sequence.hpp>
+#include <boost/optional.hpp>
+#include <boost/optional/optional_io.hpp>
+#include <catch2/catch.hpp>
+#include <cstdlib>
+#include <durak/card.hxx>
+#include <sstream>
+#include <stdexcept>
 #include <string>
+#include <type_traits>
+
+BOOST_FUSION_DEFINE_STRUCT ((shared_class), Nested, (long, answer))
+BOOST_FUSION_DEFINE_STRUCT ((shared_class), Student, (long, answer) (bool, happy) (std::string, name) (boost::optional<int>, nothingInt) (boost::optional<bool>, nothingBool) (boost::optional<std::string>, nothingString) (shared_class::Nested, nested) (boost::optional<shared_class::Nested>, nestedOptional))
+BOOST_FUSION_DEFINE_STRUCT ((shared_class), NestedVector, (std::vector<int>, intVector) (std::vector<shared_class::Nested>, nestedVector))
+namespace shared_class
+{
+struct NestedPair
+{
+  std::pair<shared_class::Nested, shared_class::Nested> nestedPair{};
+  int i{};
+};
+}
+BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedPair, nestedPair, i)
+
+namespace shared_class
+{
+struct NestedPairOptional
+{
+  std::pair<shared_class::Nested, boost::optional<shared_class::Nested>> nestedPairOptional{};
+};
+}
+BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedPairOptional, nestedPairOptional)
+
+namespace shared_class
+{
+struct NestedPairVector
+{
+  std::vector<std::pair<shared_class::Nested, shared_class::Nested>> nestedPairVector{};
+  int i{};
+};
+}
+BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedPairVector, nestedPairVector)
+
+namespace shared_class
+{
+struct NestedVectorOptional
+{
+  std::vector<boost::optional<int>> intNestedVectorOptional{};
+  std::vector<boost::optional<std::string>> stringNestedVectorOptional{};
+  std::vector<boost::optional<shared_class::Nested>> nestedVectorOptional{};
+};
+}
+BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedVectorOptional, intNestedVectorOptional, stringNestedVectorOptional, nestedVectorOptional)
+
+namespace shared_class
+{
+struct NestedPairOptionalVector
+{
+  std::vector<std::pair<shared_class::Nested, boost::optional<shared_class::Nested>>> nestedPairOptionalVector{};
+};
+}
+BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedPairOptionalVector, nestedPairOptionalVector)
 
 namespace test
 {
