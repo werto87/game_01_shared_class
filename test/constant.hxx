d034292612ea7eaf2bcd789e4c51b55d6ddc1fd6
werto87 <huhulasmichinruhe@web.de>
2021-06-06 23:51:50 +0200

    adding confu_json

--- test/constant.hxx
+++ test/constant.hxx
@@ -14,58 +14,6 @@
 #include <string>
 #include <type_traits>
 
-BOOST_FUSION_DEFINE_STRUCT ((shared_class), Nested, (long, answer))
-BOOST_FUSION_DEFINE_STRUCT ((shared_class), Student, (long, answer) (bool, happy) (std::string, name) (boost::optional<int>, nothingInt) (boost::optional<bool>, nothingBool) (boost::optional<std::string>, nothingString) (shared_class::Nested, nested) (boost::optional<shared_class::Nested>, nestedOptional))
-BOOST_FUSION_DEFINE_STRUCT ((shared_class), NestedVector, (std::vector<int>, intVector) (std::vector<shared_class::Nested>, nestedVector))
-namespace shared_class
-{
-struct NestedPair
-{
-  std::pair<shared_class::Nested, shared_class::Nested> nestedPair{};
-  int i{};
-};
-}
-BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedPair, nestedPair, i)
-
-namespace shared_class
-{
-struct NestedPairOptional
-{
-  std::pair<shared_class::Nested, boost::optional<shared_class::Nested>> nestedPairOptional{};
-};
-}
-BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedPairOptional, nestedPairOptional)
-
-namespace shared_class
-{
-struct NestedPairVector
-{
-  std::vector<std::pair<shared_class::Nested, shared_class::Nested>> nestedPairVector{};
-  int i{};
-};
-}
-BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedPairVector, nestedPairVector)
-
-namespace shared_class
-{
-struct NestedVectorOptional
-{
-  std::vector<boost::optional<int>> intNestedVectorOptional{};
-  std::vector<boost::optional<std::string>> stringNestedVectorOptional{};
-  std::vector<boost::optional<shared_class::Nested>> nestedVectorOptional{};
-};
-}
-BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedVectorOptional, intNestedVectorOptional, stringNestedVectorOptional, nestedVectorOptional)
-
-namespace shared_class
-{
-struct NestedPairOptionalVector
-{
-  std::vector<std::pair<shared_class::Nested, boost::optional<shared_class::Nested>>> nestedPairOptionalVector{};
-};
-}
-BOOST_FUSION_ADAPT_STRUCT (shared_class::NestedPairOptionalVector, nestedPairOptionalVector)
-
 namespace test
 {
 
